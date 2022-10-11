06cf022277ac5e94d71f6a4be7a862c023c98e06
aleja <aleja@DESKTOP-P3HJPHG>
2022-10-11 11:07:02 +0100

    Kata versión 3

--- src/main/java/kata1/kata1/Kata1.java
+++ src/main/java/kata1/kata1/Kata1.java
@@ -1,15 +1,17 @@
 package kata1.kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Month;
+
 
 public class Kata1 {
     
     public static void main(String[] args){
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1949,9,24);
-        Person person = new Person("Blas", date);
+        LocalDate.now();
+        LocalDate nacimiento = LocalDate.of(1949, Month.MARCH, 24);
+        //date = GregorianCalendar.getInstance();
+        //date.set(1949,9,24);
+        Person person = new Person("Blas", nacimiento);
         System.out.println(person.getName()+ " tiene "+ person.getAge()+ " años");
         
     }
